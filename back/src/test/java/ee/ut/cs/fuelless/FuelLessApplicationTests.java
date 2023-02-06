29808582c13e182a8ea0a577ee8d4b9cf30c38de
kaurvali <hullumeelne.vali@gmail.com>
2023-02-06 13:29:02 +0200

    Correct Angular + Spring setup

---
+++ back/src/test/java/ee/ut/cs/fuelless/FuelLessApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ut.cs.fuelless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelLessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
