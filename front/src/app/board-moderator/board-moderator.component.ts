3e8c0d58868b7be41233f9bece127666776aabdb
kaurvali <hullumeelne.vali@gmail.com>
2023-03-01 21:58:52 +0200

    Convert from css to scss

--- front/src/app/board-moderator/board-moderator.component.ts
+++ front/src/app/board-moderator/board-moderator.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-board-moderator',
   templateUrl: './board-moderator.component.html',
-  styleUrls: ['./board-moderator.component.css']
+  styleUrls: ['./board-moderator.component.scss']
 })
 export class BoardModeratorComponent {
 
--- front/src/app/board-user/board-user.component.ts
+++ front/src/app/board-user/board-user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-board-user',
   templateUrl: './board-user.component.html',
-  styleUrls: ['./board-user.component.css']
+  styleUrls: ['./board-user.component.scss']
 })
 export class BoardUserComponent {
 
